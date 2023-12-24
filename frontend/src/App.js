a07e96af45fd0bdb4438c0a951603793f2c3b83a
Kumari Durga <singhdurga5698@gmail.com>
2023-12-24 06:31:35 +0530

    fixed bugs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -121,7 +121,7 @@ function App() {
                 placeholder="Search movie"
                 value={searchTerm}
                 onChange={(e) => setSearchTerm(e.target.value)}
-                className="p-2 border border-gray-300 rounded-md text-black ml-[500px]"
+                className="p-2 border border-gray-300 rounded-md text-black"
               />
               <button
                 className="ml-2 px-4 py-2 rounded-md bg-blue-200"
